a74f20bb172c414a2761f3deaba86106f227bdf2
Matheus <164927300+mathersweeknd@users.noreply.github.com>
2024-12-06 14:50:42 -0300

    Delete ConexaoDB directory

--- ConexaoDB/app/models.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
